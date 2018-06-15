501a8c62553a64e23f61299d47aa5bba8b2f28a9
zhenyugu <erickool1992@live.com>
2018-06-15 13:18:35 +0800

    Initial the vue project

---
+++ booktime/src/components/Booktime.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+   <p>book time</p>
+    <ul>
+      <li>
+        <p>一地鸡毛</p>
+      </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'booktime',
+  props: {
+  }
+}
+</script>
